2ac7354a4df118d572acf858d97e6f2a56bba326
amr mahmoud <fifamahmoud9@gmail.com>
2023-07-13 16:24:57 -0700

    3

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,13 @@
 #include<stdio.h>
+/**
+ * main - entry point 
+ *
+ * Descriptions : this program to print first masseg in c 
+ * 
+ * Return : 0(success)
+ */
 int main(void)
 {
-puts("\"Programming is like building a multilingual puzzle\n");
+puts("\"Programming is like building a multilingual puzzle");
 return (0);
 }

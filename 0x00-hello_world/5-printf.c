5a5b462a9d838b1646758a0df6c30bae6d1e7c43
amr mahmoud <fifamahmoud9@gmail.com>
2023-07-13 16:29:22 -0700

    1

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,4 +1,15 @@
-#include<stdio.h>
-int main(){
-printf("with proper grammar, but the outcome is a piece of art,");
-return 0;}
+#include <stdio.h>
+
+/**
+ * main - Entry Point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+
+printf("with proper grammar, but the outcome is a piece of art,\n");
+
+return (0);
+}

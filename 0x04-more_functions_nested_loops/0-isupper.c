c1abbbffe1bc535116c441526c07d0688b29880b
Amrmahmoud99 <fifamahmoud9@gmail.com>
2023-07-20 15:53:44 -0700

    1

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,11 +1,11 @@
 #include "main.h"
-/*
- * _isupper : function return 1 if upper return 0 if else
+/**
+ * _isupper : return 1 if upper return 0 if else
  *
  * @c : input
  *
- * return : 1 if it is upper . else return 0
- */
+ * return : return 1 if upper else return 0
+ **/
 int _isupper(int c)
 {
 if (c >= 65 && c <= 90)

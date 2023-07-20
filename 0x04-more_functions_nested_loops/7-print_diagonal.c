9d722516359c4f3425c0c01c9ff0a42f55408dc4
Amrmahmoud99 <fifamahmoud9@gmail.com>
2023-07-20 16:17:02 -0700

    1

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,25 @@
+#include "main.h"
+/**
+*print_diagonal - prints a diagonal
+*@n:parameter
+*Return:returns nothing
+*/
+void print_diagonal(int n)
+{
+int ln, space;
+if (n > 0)
+{
+for (ln = 0; ln < n; ln++)
+{
+for (space = 0; space < ln; space++)
+_putchar(' ');
+
+_putchar('\\');
+
+if (len == (n - 1))
+continue;
+_putchar('\n');
+}
+}
+_putchar('\n');
+}
